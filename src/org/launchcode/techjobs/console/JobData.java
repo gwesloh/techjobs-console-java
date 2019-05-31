d82663ce62c61a40faaf780993e9dc752607f486
GREG WESLOH <GWESLOH@sbcglobal.net>
2019-05-31 11:42:33 -0500

    completed console

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -123,6 +124,28 @@ public class JobData {
             System.out.println("Failed to load job data");
             e.printStackTrace();
         }
+    }
+
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value){
+        loadData();
+
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList();
+
+        for(HashMap<String, String> job : allJobs){
+            for(Map.Entry<String, String> key : job.entrySet()){
+                if(key.getValue().toLowerCase().contains(value.toLowerCase())){
+                    jobs.add(job);
+                    break;
+                }
+            }
+
+             
+
+
+        }
+        return jobs;
     }
 
 }
